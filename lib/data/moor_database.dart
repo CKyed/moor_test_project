de8ad498f9826775ba723ff6bc8dfa47168580a8
StationærPC <christiankyed@hotmail.com>
2021-07-06 13:11:51 +0200

    ...

--- lib/data/moor_database.dart
+++ lib/data/moor_database.dart
@@ -4,23 +4,20 @@ import 'package:moor_flutter/moor_flutter.dart';
 part 'moor_database.g.dart';
 
 class Tasks extends Table {
-  IntColumn get id => integer().autoIncrement().call();
+  IntColumn get id => integer().autoIncrement()();
 
-  TextColumn get name => text()
-      .withLength(
-        min: 1,
-        max: 50,
-      )
-      .call();
+  TextColumn get name => text().withLength(min: 1, max: 50)();
+
+  Set<Column> get primaryKey => {id, name};
 }
 
 @UseMoor(tables: [Tasks])
-class AppDatabase extends _$AppDatabase {
+class AppDatabase extends _$AppDataBase {
   AppDatabase()
-      : super(FlutterQueryExecutor.inDatabaseFolder(
+      : super((FlutterQueryExecutor.inDatabaseFolder(
           path: 'db.sqlite',
           logStatements: true,
-        ));
+        )));
 
   @override
   int get schemaVersion => 1;
